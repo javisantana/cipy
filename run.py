aaa7ef0b6cca5cdb7c74e701ad42d42427af6868
javi <globalx@localhost.(none)>
2009-10-21 16:55:11 +0200

    solved problems with process management (killing zombies)

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ scm_cmds = { 'git':
                   {   
                     'reset':["git", "reset", "--hard"], 
                     'rev':["git", "rev-parse", "HEAD"] 
-                  }
+                  },
                 'svn': 
                   {   
                     'reset':["svn", "update"], 
@@ -56,8 +56,11 @@ def exec_ci_cmd(c):
   
 @route('/build')
 def build(web):
-  ret, data = cmd(scm_cmds[repo_type]['rev'], repo_path);
-  b = Build(date=datetime.datetime.now().strftime("%b%d %H:%M"), finished=False, rev=data)
+
+  kill_zombies(); #hack
+
+  data, ret = cmd(scm_cmds[repo_type]['rev'], repo_path);
+  b = Build(date=datetime.datetime.now().strftime("%b%d %H:%M"), finished=False, rev=data[:6])
   b.save();
   # i was using a thread before but sqlite doesn't support access to same object from different threads
   pid = os.fork();
@@ -77,13 +80,20 @@ def build(web):
       exec_ci_cmd("build_pass");
     else:
       exec_ci_cmd("build_failed");
-    sys.exit()
-  else:
-    return "scheduled!"
+    os._exit(0);
+  return "scheduled!"
  
+def kill_zombies():
+  try:
+    while 1:
+            os.waitpid(0, 0);
+  except:
+    pass
 
 @route('/')
 def index(web):
+  # terminate pending forked process
+  kill_zombies();
   builds = find(Build).order_by(Build.id.desc()).limit(10).all();
   template("index.html", { 'builds': builds, 'project_path': repo_path })
   
